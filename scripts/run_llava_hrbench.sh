2e3193ad5a6facbab6dae2a6253923a104f0e30e
AlyssaYoung <pinci_yang@outlook.com>
2025-11-28 10:50:37 +0800

    performance reproduction v1

--- scripts/run_llava_hrbench.sh
+++ scripts/run_llava_hrbench.sh
@@ -20,10 +20,28 @@ processed_dataset=HRBench4K_single
 work_dir=./outputs/${MODEL_NAME}
 PROCESSED_IMAGE_PATH=$work_dir/${dataset}/images
 mkdir -p $work_dir
-torchrun --nproc-per-node=$GPU --master_port 29501 run.py --data $processed_dataset --model $MODEL_NAME --judge chatgpt-0125 --work-dir $work_dir --model_path $MODEL_PATH --is_process_image --processed_image_path $PROCESSED_IMAGE_PATH --rag_model_path $RAG_MODEL_PATH --vision_tower_path $VISION_TOWER_PATH --max_step 200
-torchrun --nproc-per-node=$GPU --master_port 29501 run.py --data $dataset --model $MODEL_NAME --judge chatgpt-0125 --work-dir $work_dir --model_path $MODEL_PATH --processed_image_path $PROCESSED_IMAGE_PATH --rag_model_path $RAG_MODEL_PATH --vision_tower_path $VISION_TOWER_PATH
+# torchrun --nproc-per-node=$GPU --master_port 29501 run.py --data $processed_dataset --model $MODEL_NAME --judge chatgpt-0125 --work-dir $work_dir --model_path $MODEL_PATH --is_process_image --processed_image_path $PROCESSED_IMAGE_PATH --rag_model_path $RAG_MODEL_PATH --vision_tower_path $VISION_TOWER_PATH --max_step 200
+# torchrun --nproc-per-node=$GPU --master_port 29501 run.py --data $dataset --model $MODEL_NAME --judge chatgpt-0125 --work-dir $work_dir --model_path $MODEL_PATH --processed_image_path $PROCESSED_IMAGE_PATH --rag_model_path $RAG_MODEL_PATH --vision_tower_path $VISION_TOWER_PATH
 
+python run.py --data $processed_dataset \
+              --model $MODEL_NAME \
+              --judge chatgpt-0125 \
+              --work-dir $work_dir \
+              --model_path $MODEL_PATH \
+              --is_process_image \
+              --processed_image_path $PROCESSED_IMAGE_PATH \
+              --rag_model_path $RAG_MODEL_PATH \
+              --vision_tower_path $VISION_TOWER_PATH \
+              --max_step 200
 
+python run.py --data $dataset \
+              --model $MODEL_NAME \
+              --judge chatgpt-0125 \
+              --work-dir $work_dir \
+              --model_path $MODEL_PATH \
+              --processed_image_path $PROCESSED_IMAGE_PATH \
+              --rag_model_path $RAG_MODEL_PATH \
+              --vision_tower_path $VISION_TOWER_PATH
 # dataset=HRBench8K
 # processed_dataset=HRBench8K_single
 # work_dir=./outputs/${MODEL_NAME}
